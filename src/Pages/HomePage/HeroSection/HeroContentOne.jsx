730824e27d34efdb6fd1eab8ece7df35cce2ae41
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-05-29 17:31:53 +0600

    make a footer

--- src/Pages/HomePage/HeroSection/HeroContentOne.jsx
+++ src/Pages/HomePage/HeroSection/HeroContentOne.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 const HeroContentOne = () => {
     return (
         <div className="items-center grid-cols-2 gap-10 md:grid container pt-14 mx-auto">
-      <div className="text-center md:text-left bg-white/70 text-black p-5">
-        <p>Welcome To </p>
+      <div className="text-center md:text-left md:bg-white/70 md:text-black p-5">
+        <p>Welcome to </p>
         <h1 className="my-3 md:text-5xl text-4xl font-bold uppercase"> Ark Power Limited</h1>
         <p className="">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium libero fugiat magnam. Natus fugit deserunt dignissimos blanditiis id tempore praesentium veritatis? Ad, rerum. Veniam, iusto illo dolorum recusandae sint dolorem?
