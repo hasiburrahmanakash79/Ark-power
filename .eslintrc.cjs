27e95698073fc2b0690b3ba128d435ed8ac8b313
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-05-30 16:17:26 +0600

    make about section

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,9 @@ module.exports = {
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
+  
   rules: {
+    "react/no-unescaped-entities": 0,
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,10 +1,51 @@
-
 const About = () => {
-    return (
-        <div>
-           <h1>Welcome to Ark Power Limited</h1> 
-        </div>
-    );
+  return (
+    <div className="grid grid-cols-2 p-5 gap-12">
+      <div>
+        <h1 className="text-4xl uppercase font-bold mb-5">Ark Power Limited</h1>
+        <p>
+          Ark Power, a prominent integrated Power, Electronic, and Communication
+          technology company, has positioned itself as a leader in Bangladesh's
+          energy landscape. Founded with a commitment to enhancing power
+          protection and generation across various sectors, Ark Power has
+          established itself as a reliable provider of related equipment.
+          Strategically located in Banani, Dhaka, the capital city of
+          Bangladesh, Ark Power enjoys a significant presence in the country.
+          Over the years, the company has achieved substantial success,
+          solidifying its position as a trusted brand in the power industry.
+          With a vision to become a distinguished brand in professional EPC
+          (Engineering, Procurement, and Construction) of power generation
+          projects, Ark Power emphasizes timely delivery, quality enhancement,
+          and customer satisfaction. Ark Power's core competency lies in
+          offering comprehensive energy solutions with cost-effective equipment
+          tailored for enterprises and projects. Moreover, the company
+          distinguishes itself through professional after-sales services,
+          managed by a competent and trained technical team. Innovation is
+          central to Ark Power's operations, as evidenced by its involvement in
+          research and development initiatives aimed at producing power without
+          fuel. Additionally, the company is committed to promoting smart
+          technologies that contribute to lower energy consumption across
+          various equipment. Ark Power's commitment to customer satisfaction
+          extends beyond product delivery. The company stands out for its
+          professional services, offering troubleshooting assistance for any
+          supplied generators, power plants, or equipment, along with the
+          provision of necessary spare parts. Confident in the quality of its
+          products and services, Ark Power aims to become a trustworthy power
+          service provider, addressing all power-related concerns in Bangladesh
+          and beyond. Through its dedication to excellence and innovation, Ark
+          Power continues to make significant strides in shaping the future of
+          the energy sector.
+        </p>
+        
+      </div>
+      <div className="drop-shadow-2xl ">
+        <img
+          src="https://www.arkpowerltd.com.bd/assets/images/arkpower/ISO_Ark_Power_Ltd.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default About;
