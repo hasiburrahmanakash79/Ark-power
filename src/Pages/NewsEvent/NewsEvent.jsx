c3a55fcd04b40362b55b3d047ffeffb35fc7b978
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-06-04 15:11:20 +0600

    start news and event page

--- src/Pages/NewsEvent/NewsEvent.jsx
+++ src/Pages/NewsEvent/NewsEvent.jsx
@@ -1,11 +1,13 @@
-
-
 const NewsEvent = () => {
-    return (
-        <div>
-            <h1>NewsEvent</h1>
-        </div>
-    );
+  return (
+    <div className="min-h-screen container mx-auto bg-slate-50 flex items-center justify-center p-6">
+      <div className="mt-24 ">
+        <h1 className="text-primary text-center text-5xl uppercase">
+          News & Events
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default NewsEvent;
