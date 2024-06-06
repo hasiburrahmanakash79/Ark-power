f0d77ac2e20d0f079a108d05b9ee1ec4d26e4d21
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-06-06 10:13:50 +0600

    make two button on table hor remove and edit date

--- src/Pages/AdminDashboard/ManageProducts/ManageProducts.jsx
+++ src/Pages/AdminDashboard/ManageProducts/ManageProducts.jsx
@@ -44,13 +44,12 @@ const ManageProducts = () => {
                 </td>
                 <td className="p-2 md:p-4">
                   <Typography variant="small" color="blue-gray" className="font-normal text-xs md:text-base">
-                    {description.slice(0, 100)}
+                    {description.slice(0, 50)}
                   </Typography>
                 </td>
-                <td className="p-2 md:p-4">
-                  <Typography as="a" href="#" variant="small" color="blue-gray" className="font-medium text-xs md:text-base">
-                    Edit
-                  </Typography>
+                <td className="p-2 flex gap-5 md:p-4 text-black">
+                  <button className="btn bg-orange-400 p-2">Edit</button>
+                  <button className="btn bg-red-400 p-2">Delete</button>
                 </td>
               </tr>
             ))}
