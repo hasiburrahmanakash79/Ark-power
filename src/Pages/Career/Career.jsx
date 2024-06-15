132b6ef2a76b2039f90e050c46c7260a718672d0
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-06-15 22:38:46 +0600

    make a career section

--- src/Pages/Career/Career.jsx
+++ src/Pages/Career/Career.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Career = () => {
   return (
     <div>
-      <div className="bg-[url('https://images.squarespace-cdn.com/content/v1/5876d8d6e3df28c4d83ae377/1485224208084-2QZTQMXNZGNF72C4MWMO/dhaka+new+market+rickshaws+2.jpg')] bg-cover">
-        <div className="p-5 pt-32 bg-black/20 text-white">
+      <div className="bg-[url('https://neevfund.com/wp-content/uploads/2023/03/career-banner.jpg')] bg-cover">
+        <div className="p-5 pt-32 bg-white/40 text-primary">
           <h2 className="text-2xl container mx-auto md:text-6xl font-semibold uppercase mt-14 md:mt-32">
             CAREER
           </h2>
