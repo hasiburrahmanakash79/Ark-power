d137aa27e9451ac2ecdb9512ee436b0770b5d0ca
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-06-27 10:47:40 +0600

    make career detail page resposive

--- src/Pages/Career/CareerDetails.jsx
+++ src/Pages/Career/CareerDetails.jsx
@@ -14,14 +14,14 @@ const CareerDetails = () => {
   }
   return (
     <div className="container mx-auto p-5 md:mt-24 mt-14">
-      <div className="grid md:grid-cols-4">
-        <div className="col-span-1">
+      <div className="grid md:grid-cols-4 gap-10">
+        <div className="md:col-span-1">
           <p className="text-3xl font-semibold uppercase mb-5">
             {careerDetail.category}
           </p>
           <Link to='/career' className="p-3 border-2 uppercase font-semibold rounded border-black hover:bg-black hover:text-white">Back to Career Page</Link>
         </div>
-        <div className="col-span-3">
+        <div className="md:col-span-3">
           <h1 className="text-3xl font-semibold mb-5">{careerDetail.title}</h1>
           <div
             dangerouslySetInnerHTML={{
