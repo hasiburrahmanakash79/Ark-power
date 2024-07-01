e7cfcba9644f5a8d7d30a0c4580683d6ffeae588
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-07-01 12:52:06 +0600

    make auth provider

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,73 @@
+import { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import axios from "axios";
+import app from "../firebase/firebase.config";
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+
+  const signUpUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const logInUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const updateUserInfo = (name, photo) =>
+    updateProfile(auth.currentUser, { displayName: name, photoURL: photo });
+
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      if (currentUser) {
+        axios.post("http://localhost:3000/jwt", { email: currentUser.email })
+          .then((data) => {
+            const token = data.data.token;
+            localStorage.setItem("access-token", token);
+            setLoading(false);
+          });
+      } else {
+        localStorage.removeItem("access-token");
+      }
+      
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    signUpUser,
+    logInUser,
+    updateUserInfo,
+    logOut,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
