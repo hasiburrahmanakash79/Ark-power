12b5ab04a384a8f2e0b26f88f5eba25a099aef1b
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-07-04 13:22:53 +0600

    make a admin route sequre the admin dashboard

--- src/Pages/HomePage/HeroSection/Hero.jsx
+++ src/Pages/HomePage/HeroSection/Hero.jsx
@@ -8,7 +8,7 @@ import HeroContentOne from "./HeroContentOne";
 
 const Hero = () => {
   return (
-    <div className="">
+    <div className=" h-screen">
       <Swiper
         className="mySwiper "
         pagination={{
@@ -23,28 +23,28 @@ const Hero = () => {
       >
         <SwiperSlide>
           <div className="bg-[url('https://t4.ftcdn.net/jpg/05/83/56/37/360_F_583563752_xDMCBAridvFzOCjEBJKjiZKXjtJIfZQF.jpg')] bg-cover">
-            <div className="md:ps-32 md:pb-20 pt-32 bg-black text-white bg-opacity-50">
+            <div className="min-h-screen bg-black text-white bg-opacity-50">
               <HeroContentOne/>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="bg-[url('https://tafepower.com/assets/images/tafe-power-genset-banner-1.jpg')] bg-cover">
-            <div className="md:ps-32 md:pb-20 pt-32 bg-black text-white bg-opacity-50">
+            <div className="min-h-screen bg-black text-white bg-opacity-50">
             <HeroContentOne/>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="bg-[url('https://www.pramaindia.in/wp-content/uploads/2023/06/Website_Banner-17-min.jpg')] bg-cover">
-            <div className="md:ps-32 md:pb-20 pt-32 bg-black text-white bg-opacity-50">
+            <div className="min-h-screen bg-black text-white bg-opacity-50">
             <HeroContentOne/>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="bg-[url('https://www.honnypower.com/Uploads/201705/banner01.jpg')] bg-cover">
-            <div className="md:ps-32 md:pb-20 pt-32 bg-black text-white bg-opacity-50">
+            <div className="min-h-screen bg-black text-white bg-opacity-50">
             <HeroContentOne/>
             </div>
           </div>
