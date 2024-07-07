fd3744a4ef13809cf2303af9b4006bea4d149166
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-07-07 17:29:52 +0600

    admin dashboard

--- src/Pages/AdminDashboard/AdminHome/AdminHome.jsx
+++ src/Pages/AdminDashboard/AdminHome/AdminHome.jsx
@@ -10,6 +10,24 @@ const AdminHome = () => {
             alt=""
           />
         </div>
+
+        <div className="my-10 grid grid-cols-5 gap-5">
+          <div className="border shadow-lg rounded-lg">
+            <h1 className="p-5">Lorem ipsum dolor sit amet.</h1>
+          </div>
+          <div className="border shadow-lg rounded-lg">
+            <h1 className="p-5">Lorem ipsum dolor sit amet.</h1>
+          </div>
+          <div className="border shadow-lg rounded-lg">
+            <h1 className="p-5">Lorem ipsum dolor sit amet.</h1>
+          </div>
+          <div className="border shadow-lg rounded-lg">
+            <h1 className="p-5">Lorem ipsum dolor sit amet.</h1>
+          </div>
+          <div className="border shadow-lg rounded-lg">
+            <h1 className="p-5">Lorem ipsum dolor sit amet.</h1>
+          </div>
+        </div>
         <div>
         </div>
       </div>
